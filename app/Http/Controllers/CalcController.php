1eefe9e0b22c292967a9978c6a150030b5149318
piezoh <noriko.piezoh@gmail.com>
2023-08-29 09:42:09 +0900

    計算アプリ課題完了

---
+++ app/Http/Controllers/CalcController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CalcController extends Controller
+{
+    public function result($num1, $operator, $num2) {
+
+        if ($operator === 'addition') {
+
+            $result = $num1 + $num2;
+
+        } elseif ($operator === 'subtraction') {
+        
+            $result =  $num1 - $num2;
+
+        } elseif ($operator === 'multiplication') {
+
+            $result =  $num1 * $num2;
+        
+        } elseif ($operator === 'division') {
+
+            $result =  $num1 / $num2;
+
+        }
+
+
+        return view('calc.calcresult', ['result' => $result]);
+    }
+}
